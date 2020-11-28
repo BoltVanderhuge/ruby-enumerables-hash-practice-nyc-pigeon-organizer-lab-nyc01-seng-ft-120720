7ec60e40bd02771830fff735b90f535cabe7a4fe
John Wisneski <john.j.wisneski@gmail.com>
2020-11-28 18:48:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,19 +5,14 @@ def nyc_pigeon_organizer(data)
       names.each do |name|
         if !final_array[name]
           final_array[name] = {}
-    end
-    if !final_array[name][key]
-      !final_array[name][key] = []
-    end
+        end
+        if !final_array[name][key]
+          !final_array[name][key] = []
+        end
     !final_array[name][key].push(inner_key.to_s)
     end
   end
     final_array
   end
   
-  
-  
-  
-  
-  
-end
+  end
